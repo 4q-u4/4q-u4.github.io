c7ef6fdd6ffebe8b327ad465dbd9f76eae2f9caa
daniellmadi <se.danielmadi@gmail.com>
2023-09-20 00:45:31 +0300

    homepage update

--- script.js
+++ script.js
@@ -1,64 +1,31 @@
-const textToType = "Cybersecurity Specialist";
-const typedTextElement = document.querySelector(".typed-text");
 
-function typeText() {
-    let index = 0;
-    function type() {
-        if (index < textToType.length) {
-            typedTextElement.textContent += textToType.charAt(index);
-            index++;
-            setTimeout(type, 100); // Adjust typing speed here (in milliseconds)
+
+const specializationText = 'Cybersec specialist'; // Remove the leading space and double quotes
+const specializationElement = document.getElementById("specialization");
+let currentIndex = 0;
+let isDeleting = false;
+let typingSpeed = 200; // Adjust typing speed (milliseconds)
+
+function typeSpecialization() {
+    const currentText = specializationElement.textContent;
+    if (!isDeleting) {
+        specializationElement.innerHTML = '{"' + specializationText.substring(0, currentIndex + 1) + '"<span class="cursor"></span>}';
+        currentIndex++;
+        if (currentIndex === specializationText.length + 2) {
+            isDeleting = true;
+            typingSpeed = 250; // Faster deletion speed (adjust as needed)
+        }
+    } else {
+        specializationElement.innerHTML = '{"' + specializationText.substring(0, currentIndex - 1) + '"<span class="cursor"></span>}';
+        currentIndex--;
+        if (currentIndex === 0) {
+            isDeleting = false;
+            typingSpeed = 200; // Typing speed after deletion
         }
     }
-    type();
+
+    setTimeout(typeSpecialization, typingSpeed);
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    typeText();
-});
-
-
-
-// Wait for the document to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the cloud element
-    const cloud = document.querySelector(".cloud");
-
-    // Add animation class to the cloud
-    cloud.classList.add("cloud-animation");
-
-    // Show the cloud
-    cloud.style.display = "block";
-
-    // After a delay, hide the cloud
-    setTimeout(() => {
-        cloud.style.display = "none";
-    }, 3000); // Adjust the delay as needed
-});
-
-
-// JavaScript to trigger the continuous animation
-const heading = document.getElementById('portfolio-heading');
-
-textToType = 'Portfoli I';
-let isTyping = false;
-
-function toggleAnimation() {
-    if (isTyping) {
-        heading.classList.remove('typing');
-        setTimeout(() => {
-            heading.textContent = '';
-            heading.style.width = '0';
-            isTyping = false;
-        }, 500);
-    } else {
-        heading.textContent = textToType;
-        heading.style.width = 'auto';
-        heading.classList.add('typing');
-        isTyping = true;
-    }
-}
-
-// Start the continuous animation
-setInterval(toggleAnimation, 3000); // Change text every 3 seconds
-toggleAnimation(); // Start the animation immediately
+// Start typing animation
+setTimeout(typeSpecialization, typingSpeed);
